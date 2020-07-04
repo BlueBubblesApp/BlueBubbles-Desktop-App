283aae53c31c5c35d262683672d7dc4ea684eddd
Zach Shames <zach.shames@threatq.com>
2020-07-04 11:23:47 -0400

    Formatting

--- src/renderer/components/ViewContainer/MessagingView/LeftCol/LeftCol.tsx
+++ src/renderer/components/ViewContainer/MessagingView/LeftCol/LeftCol.tsx
@@ -1,31 +1,32 @@
 import * as React from "react";
 import "./LeftCol.css";
+import { ipcRenderer } from "electron";
 import LeftTopNav from "./TopNav/LeftTopNav";
 import LeftConversationNav from "./ConversationsNav/LeftConversationsNav";
 import BottomLeftNav from "./BottomNav/BottomLeftNav";
-import { ipcRenderer } from "electron";
 
-interface LeftColProps{
+interface LeftColProps {
     theme: string;
 }
 
 class LeftCol extends React.Component<object, LeftColProps> {
-    constructor(props){
-        super(props)
+    constructor(props) {
+        super(props);
 
         this.state = {
             theme: ""
-        }
+        };
     }
 
-    async componentDidMount(){
-        const config = await ipcRenderer.invoke("get-config")
-        this.setState({theme: config.theme})
+    async componentDidMount() {
+        const config = await ipcRenderer.invoke("get-config");
+        this.setState({ theme: config.theme });
 
         ipcRenderer.on("config-update", (_, args) => {
-            this.setState({theme: args.theme});
+            this.setState({ theme: args.theme });
         });
     }
+
     render() {
         return (
             <div className="LeftCol-Mes" data-theme={this.state.theme}>
--- src/renderer/components/ViewContainer/SettingsView/RightCol/RightCol.tsx
+++ src/renderer/components/ViewContainer/SettingsView/RightCol/RightCol.tsx
@@ -1,28 +1,27 @@
 import * as React from "react";
 import "./RightCol.css";
-import SettingDisplay from "./SettingDisplay/SettingDisplay";
 import { ipcRenderer } from "electron";
+import SettingDisplay from "./SettingDisplay/SettingDisplay";
 
 interface RightColProps {
     theme: string;
 }
 
-class RightCol extends React.Component<object, RightColProps>{
-    constructor(props){
-        super(props)
+class RightCol extends React.Component<object, RightColProps> {
+    constructor(props) {
+        super(props);
 
         this.state = {
             theme: ""
-        }
+        };
     }
 
-    async componentDidMount(){
-        const config = await ipcRenderer.invoke("get-config")
-        this.setState({theme: config.theme})
+    async componentDidMount() {
+        const config = await ipcRenderer.invoke("get-config");
+        this.setState({ theme: config.theme });
 
         ipcRenderer.on("config-update", (_, args) => {
-
-            this.setState({theme: args.theme});
+            this.setState({ theme: args.theme });
         });
     }
 
