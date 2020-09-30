9d5fc9e32e2fa7bb135107f9f4db8f8b402177d8
Maxwell Fortney <maxwellfortney@gmail.com>
2020-09-30 00:23:20 -0400

    Added Ui for image sending

--- src/renderer/components/ViewContainer/MessagingView/RightCol/Messaging/BottomNav/RightBottomNav.tsx
+++ src/renderer/components/ViewContainer/MessagingView/RightCol/Messaging/BottomNav/RightBottomNav.tsx
@@ -1,11 +1,15 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react/sort-comp */
 /* eslint-disable max-len */
 /* eslint-disable class-methods-use-this */
 import * as React from "react";
 import * as path from "path";
+import * as fs from "fs";
 import { ipcRenderer } from "electron";
 import { Attachment, Chat, Message } from "@server/databases/chat/entity";
 import { generateUuid } from "@renderer/helpers/utils";
+import CloseIcon from "@renderer/components/TitleBar/close.png";
+// import Monkey from "C://Users/Maxwell/Downloads/1.jpeg";
 
 import "./RightBottomNav.css";
 
@@ -57,6 +61,10 @@ class RightBottomNav extends React.Component<Props, State> {
                 // Do nothing
             }
         });
+
+        ipcRenderer.on("set-current-chat", () => {
+            this.setState({ attachmentPaths: [] });
+        });
     }
 
     handleMessageChange = event => {
@@ -78,6 +86,7 @@ class RightBottomNav extends React.Component<Props, State> {
             dateCreated: new Date()
         });
 
+        const newAttachmentPaths = new Array<string>();
         for (const aPath of this.state.attachmentPaths) {
             // Create the attachment
             const attachment: Attachment & { filepath: string } = await ipcRenderer.invoke("create-attachment", {
@@ -99,7 +108,11 @@ class RightBottomNav extends React.Component<Props, State> {
             // ipcRenderer.invoke("send-to-ui", { event: "add-message", contents: assocMsg });
             // ipcRenderer.invoke("save-message", { chat: this.props.chat, message: assocMsg });
             ipcRenderer.invoke("send-message", { chat: this.props.chat, message: assocMsg });
+            if (!this.state.attachmentPaths.includes(aPath)) {
+                newAttachmentPaths.push(aPath);
+            }
         }
+        this.setState({ attachmentPaths: newAttachmentPaths });
 
         // Send the main text
         if (textMessage && textMessage.length > 0) {
@@ -120,11 +133,33 @@ class RightBottomNav extends React.Component<Props, State> {
         sendAudio.play();
     }
 
-    handleAddAttachment() {
-        dialog.showOpenDialog({
+    async handleAddAttachment() {
+        const dialogReturn = await dialog.showOpenDialog({
             properties: ["openFile"],
             filters: [{ name: "Images", extensions: ["jpg", "jpeg", "png"] }]
         });
+
+        const chosenFiles = dialogReturn.filePaths;
+        const attachmentPathsCopy = this.state.attachmentPaths;
+
+        chosenFiles.forEach(filePath => {
+            // const output = fs.readFileSync(filePath).toString("base64");
+            // console.log(output);
+            attachmentPathsCopy.push(filePath);
+        });
+        console.log(attachmentPathsCopy);
+        this.setState({ attachmentPaths: attachmentPathsCopy });
+    }
+
+    openAttachment(attachmentPath) {
+        ipcRenderer.invoke("open-attachment", attachmentPath);
+    }
+
+    removeAttachment(attachmentPath) {
+        const x = this.state.attachmentPaths;
+        x.splice(x.indexOf(attachmentPath, 0), 1);
+        console.log(x);
+        this.setState({ attachmentPaths: x });
     }
 
     handleRecordEnter() {
@@ -206,12 +241,33 @@ class RightBottomNav extends React.Component<Props, State> {
                     </div>
                 ) : (
                     <>
-                        <div id="leftAttachmentButton" onClick={this.handleAddAttachment}>
+                        <div id="leftAttachmentButton" onClick={() => this.handleAddAttachment()}>
                             <svg id="attachIcon" viewBox="0 0 25 25">
                                 <path d="M7.46,25a7.57,7.57,0,0,1-5.19-2l-.09-.08a6.72,6.72,0,0,1,0-9.9L15,1.42a5.46,5.46,0,0,1,7.35,0A4.88,4.88,0,0,1,24,5a4.83,4.83,0,0,1-1.56,3.54L10.38,19.41A3.23,3.23,0,0,1,6,19.4a2.91,2.91,0,0,1,0-4.3L17.27,5l1.33,1.49L7.35,16.57a.91.91,0,0,0-.29.66.93.93,0,0,0,.31.68,1.23,1.23,0,0,0,1.66,0L21.09,7.11a2.81,2.81,0,0,0,0-4.16,3.45,3.45,0,0,0-4.69-.06L3.53,14.46a4.72,4.72,0,0,0,0,7l.09.08a5.65,5.65,0,0,0,7.63,0L23.33,10.69l1.34,1.49L12.62,23A7.53,7.53,0,0,1,7.46,25Z" />
                             </svg>
                         </div>
                         <div id="messageField">
+                            {this.state.attachmentPaths.length > 0
+                                ? this.state.attachmentPaths.map(filePath => (
+                                      <div className="aNewAttachmentDiv" key={filePath}>
+                                          <div>
+                                              <img
+                                                  className="aNewAttachment"
+                                                  src={`data:image;base64,${fs
+                                                      .readFileSync(filePath)
+                                                      .toString("base64")}`}
+                                                  onClick={() => this.openAttachment(filePath)}
+                                              />
+                                              <p>{filePath.replace(/^.*[\\/]/, "")}</p>
+                                          </div>
+                                          <img
+                                              className="aNewAttachmentRemoveButton"
+                                              src={CloseIcon}
+                                              onClick={() => this.removeAttachment(filePath)}
+                                          />
+                                      </div>
+                                  ))
+                                : null}
                             <input
                                 id="messageFieldInput"
                                 type="text"
